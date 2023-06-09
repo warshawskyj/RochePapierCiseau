2516b45378e25cab0169c6083ade000bc09dbe52
warshawskyj <112487742+warshawskyj@users.noreply.github.com>
2023-06-09 09:58:33 -0400

    Ajout des commentaires

--- attack_animations.py
+++ attack_animations.py
@@ -2,29 +2,26 @@ from enum import Enum
 from arcade import Sprite
 
 
-class AttackType(Enum):
-    ROCK = 0
-    PAPER = 1
-    SCISSORS = 2
-
-
 class Animation(Sprite):
     def __init__(self, interval, frames, *args, **kwargs):
         """
         Une classe qui permet de gerer automatiquement des animations.
-        :param interval: le temps entre chaque frame
-        :param frames: une liste de sprites
+        interval: le temps entre chaque frame
+        frames: une liste de sprites
         """
         super().__init__(*args, **kwargs)
         self.interval = interval
         self.frames = frames
         self.current_frame = 0
+        #la texture actuelle
         self.texture = self.frames[self.current_frame]
         self.time_since_last_frame = 0
 
     def update(self, delta_time):
         self.time_since_last_frame += delta_time
+        #il faut passer au prochain frame
         if self.time_since_last_frame > self.interval:
+            #le modulo fait en sort que les frames se répétent en boucle
             self.current_frame = (self.current_frame + 1) % len(self.frames)
             self.time_since_last_frame = 0
             self.texture = self.frames[self.current_frame]
