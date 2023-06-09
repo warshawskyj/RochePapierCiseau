4d1de0f6c18c0076a5b715bd06f6581adac4eec2
warshawskyj <112487742+warshawskyj@users.noreply.github.com>
2023-06-09 09:56:37 -0400

    Ajout des commentaires

--- rectangle.py
+++ rectangle.py
@@ -2,6 +2,9 @@ import arcade
 
 
 class Rectangle:
+    """
+    une classe possédant les caractèristiques nécessaires pour afficher un rectangle dans arcade
+    """
     def __init__(self, center_x, center_y, width, height, color=arcade.color.RED):
         self.center_x = center_x
         self.center_y = center_y
@@ -10,6 +13,7 @@ class Rectangle:
         self.height = height
 
     def draw(self):
+        #il faut soustraire la moitié des dimensions du point central pour trouver les extrémités
         arcade.draw_lrtb_rectangle_outline(self.center_x - self.width / 2, self.center_x + self.width / 2,
                                            self.center_y + self.height / 2, self.center_y - self.height / 2,
                                            self.color)
